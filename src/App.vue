149ef909b2797efc1223ecc2bbc3a1be3c70afdc
Payton  Provo <paytonprovo1@gmail.com>
2021-08-21 22:00:49 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/cats">Cats</router-link> |
+    <router-link to="/dogs">Dogs</router-link> |
   </div>
-  <router-view/>
+  <b-container>
+    <router-view/>
+  </b-container>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/sass/index.sass'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
